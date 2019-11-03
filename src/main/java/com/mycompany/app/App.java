fd0ac528bbdd1701c54229a7d4e25db6f913c0cf
andydevop <56658287+andydevop@users.noreply.github.com>
2019-11-02 23:38:14 -0500

    1137PMCST-Nov02-2019-Commit#2
    1137PMCST-Nov02-2019-Commit#2 Updated target!

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Repository created right from the local and updated through maven!" );
     }
 }
